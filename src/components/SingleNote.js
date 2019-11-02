54da602c6eae8c87835a2be63378f981e47fd5a7
Lucian Condrea <luciancondreadev@gmail.com>
2019-11-02 19:57:17 -0400

    fix title and favicon

--- src/components/SingleNote.js
+++ src/components/SingleNote.js
@@ -6,33 +6,50 @@ function SingleNote({ back }) {
     const [ note, setNote ] = useState(getRandomFrom(notes))
     const [ fret, setFret ] = useState(randUpTo(20))
     const [ playing, setPlaying ] = useState(true)
+    const [ intervalLength, setIntervalLength ] = useState(4000)
     const noteItv = useRef();
-    const fretItv = useRef();
     
     useEffect(() => {
         if (playing) {
             noteItv.current = setInterval(() => {
                 setNote(getRandomFrom(notes))
-            }, 2000)
-            fretItv.current = setInterval(() => {
-                setFret(randUpTo(20))
-            }, 8000)
+            }, intervalLength)
         }
         
         return () => {
-            clearInterval(noteItv.current)
-            clearInterval(fretItv.current) 
+            clearInterval(noteItv.current) 
         }
-    }, [playing])
+    }, [playing, intervalLength])
 
+
+    function handleTimeChange(e) {
+        const inter = Number(e.target.value)
+
+        if (inter > 0) {
+            setIntervalLength(inter)
+        }
+    }
 
     return (
         <div>
             <h1>Single Note</h1>
-            <div>Note: {note}</div>
-            <div>Frets: {fret} - {fret + 4}</div>
-            { playing ? <button onClick={() => { setPlaying(false) }}>Pause</button> :
-            <button onClick={() => { setPlaying(true) }}>Resume</button> }
+            <div>
+                Note: {note}
+                <button onClick={() => setPlaying(!playing) }>{ playing ? 'Pause' : 'Resume' }</button>
+            </div>
+            <div>
+                Frets: {fret} - {fret + 3}
+                <button onClick={() => setFret(randUpTo(20))}>Change Frets</button>
+            </div>
+            <div>
+                Change Speed (ms):
+                <input 
+                    onChange={handleTimeChange} 
+                    value={intervalLength} 
+                    type='number' 
+                    min='1000' 
+                    max='6000' />
+            </div>
             <button onClick={back}>Back</button>
         </div>
     )
