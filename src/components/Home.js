2a41c650216a9e4acaf20f134fa7731311a97236
Lucian Condrea <luciancondreadev@gmail.com>
2019-11-04 10:36:59 -0500

    center header

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home({ setScreen }) {
   return (
     <div className="page">
-      <h1 className="header" style={{ justifyContent: 'center' }}>Guitar Trainer</h1>
+      <h1 className="header">Guitar Trainer</h1>
       <button onClick={() => setScreen('single note')}>Single Note</button>
       <button onClick={() => setScreen('single scale')}>Single Scale</button>
       <button onClick={() => setScreen('progression')}>Progression</button>
