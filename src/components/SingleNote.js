fb6d42fd84074905b57e239083b91851f143c034
Lucian Condrea <luciancondreadev@gmail.com>
2019-11-04 12:15:35 -0500

    add speed selection component

--- src/components/SingleNote.js
+++ src/components/SingleNote.js
@@ -40,16 +40,32 @@ function SingleNote({ back }) {
             </div>
             <button className={redButtonClass} onClick={() => setPlaying(!playing) }>{note}</button>
             <button className="button-blue" onClick={() => setFret(randUpTo(20))}>Frets: {fret} - {fret + 3}</button>
-            {/* Previously used time change solution
-                <div>
-                Change Speed (ms):
-                <input 
-                    onChange={handleTimeChange} 
-                    value={intervalLength} 
-                    type='number' 
-                    min='1000' 
-                    max='6000' />
-            </div> */}
+            <div className="speed-form">
+                <label>Slow</label>
+                <label>Fast</label>
+                <fieldset>
+                    <div>
+                        <input type="radio" name="speed" value="1000" id="radio1"/>
+                        <label for="radio1"></label>
+                    </div>
+                    <div>
+                        <input type="radio" name="speed" value="1500" id="radio2"/>
+                        <label for="radio2"></label>
+                    </div>
+                    <div>
+                        <input type="radio" name="speed" value="2000" id="radio3"/>
+                        <label for="radio3"></label>
+                    </div>
+                    <div>
+                        <input type="radio" name="speed" value="3500" id="radio4"/>
+                        <label for="radio4"></label>
+                    </div>
+                    <div>
+                        <input type="radio" name="speed" value="5000"  id="radio5"/>
+                        <label for="radio5"></label>
+                    </div>
+                </fieldset>
+            </div>
         </div>
     )
 }
