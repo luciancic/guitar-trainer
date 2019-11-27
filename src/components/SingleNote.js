2e2faaf3101d6e09f551926257cb81f0f980d1f5
Lucian Condrea <luciancondreadev@gmail.com>
2019-11-27 16:53:31 -0500

    update guitar-trainer-lib

--- src/components/SingleNote.js
+++ src/components/SingleNote.js
@@ -1,15 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { getRandomNote } from 'guitar-trainer-lib'
+import { getRandomNote, getRandomFret } from 'guitar-trainer-lib'
 import BackButton from './BackButton'
 import SpeedSelect from './SpeedSelection'
 
-function randUpTo(int) {
-    return int;
-}
-
 function SingleNote({ back }) {
     const [ note, setNote ] = useState(getRandomNote())
-    const [ fret, setFret ] = useState(randUpTo(20))
+    const [ fret, setFret ] = useState(getRandomFret(21))
     const [ playing, setPlaying ] = useState(true)
     const [ intervalLength, setIntervalLength ] = useState(2000)
     const noteItv = useRef()
@@ -34,7 +30,7 @@ function SingleNote({ back }) {
                 <h1>Single Note</h1>
             </div>
             <button className={redButtonClass} onClick={() => setPlaying(!playing) }>{note}</button>
-            <button className="button-blue" onClick={() => setFret(randUpTo(20))}>Frets: {fret} - {fret + 3}</button>
+            <button className="button-blue" onClick={() => setFret(getRandomFret(21))}>Frets: {fret} - {fret + 3}</button>
             <SpeedSelect handleChange={setIntervalLength}/>
         </div>
     )
